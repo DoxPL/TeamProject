50cd25e9155765bdfbd4071fbf0b67639f6f663b
DoxPL <dox100@gmail.com>
2018-10-19 00:32:30 +0200

    Added Alert class, Changed dialog view

---
+++ app/src/main/java/com/example/student/teamproject/Alert.java
@@ -0,0 +1,67 @@
+package com.example.student.teamproject;
+
+public class Alert {
+    private String name;
+    private String description;
+    private int year, month, day, hour, minute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getHour() {
+
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getDay() {
+
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
